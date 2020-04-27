250324b665873c96b5c9afd3824eb6e38013be69
Malek Salem <malek.salem.14@gmail.com>
2020-04-27 17:35:10 +0000

    fix bug

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,5 +4,5 @@ word = "Holberton"
 # comment
 # comment
 print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[:2]))
+print("Last 2 letters: {}".format(word[-2:]))
 print("Middle word: {}".format(word[1:-1]))
