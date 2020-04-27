29c6a06156eb48e7e83d67df2a064c4fd570fce8
Malek Salem <malek.salem.14@gmail.com>
2020-04-27 17:49:14 +0000

    fix issue

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # comment :p
-print("{}{}{}".format(str[39:66], str[-23:-17], str[:6]))
+print(str[39:66]+str[-23:-17]+str[:6])
