7f2b7ba0b2c50b1b226cc0cf8c2f159695480c24
Malek Salem <malek.salem.14@gmail.com>
2020-04-27 17:25:51 +0000

    fix req

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# print a string
-print("Welcome to {} {}!".format(str1, str2))
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
