06ac7b302816f4b3ecc9cc427243a3a6486367af
Malek Salem <malek.salem.14@gmail.com>
2020-04-27 17:33:49 +0000

    add task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# comment
+# comment
+# comment
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[:2]))
+print("Middle word: {}".format(word[1:-1]))
