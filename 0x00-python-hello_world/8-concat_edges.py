bcc1cc44bac7663ef6fce952f8e2f6fafeb5361f
Malek Salem <malek.salem.14@gmail.com>
2020-04-27 17:48:02 +0000

    add task 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# comment :p
+print("{}{}{}".format(str[39:66], str[-23:-17], str[:6]))
