7cb14d2041ffa54d7d940ad4a3a74cf4133da104
Malek Salem <malek.salem.14@gmail.com>
2020-04-28 21:30:24 +0000

    add task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    for num in range(1,100 + 1):
+        result = ""
+
+        if num % 3 == 0: result += "Fizz"
+        if num % 5 == 0: result += "Buzz"
+        
+        if result == "": result = num
+
+        print("{}".format(result), end=" ")
