b285a43907c3ab38b58615f9e6f3d87d27804dcc
Malek Salem <malek.salem.14@gmail.com>
2020-04-28 17:04:31 +0000

    add task 0

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10, 10)
+
+if number == 0:
+    print("{:d} is zero".format(number))
+
+elif number > 0:
+    print("{:d} is positive".format(number))
+
+else:
+    print("{:d} is negative".format(number))
