57b664b28ce3b8bc3ac978bc53a98d8057c192d4
Malek Salem <malek.salem.14@gmail.com>
2020-04-28 22:40:15 +0000

    fix errors

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -7,11 +7,3 @@ def remove_char_at(str, n):
     if n < len(str) and n >= 0:
         output.pop(n)
     return "".join(output)
-
-
-
-print(remove_char_at("Holberton School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
