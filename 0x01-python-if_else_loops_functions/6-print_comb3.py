94b686aec19e5a69d625687c8822d7948ce72ff8
Malek Salem <malek.salem.14@gmail.com>
2020-04-28 19:58:20 +0000

    update code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,19 +3,16 @@
 
 def check_digit(num):
 
-    def convert(number):
-        return "{:02d}".format(number)
-
-    if int(convert(num)) > int(convert(num)[::-1]):
-        return False
-    return True
+    if num // 10 < num % 10:
+        return True
+    return False
 
 
 for i in range(1, 99):
 
     if check_digit(i):
+        
         if i != 89:
             print("{:02d}".format(i), end=", ")
-
         else:
             print(89)
