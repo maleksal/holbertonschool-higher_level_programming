3b6054d6123a60842cf5a0ff4b684b9b88a1f35c
Malek Salem <malek.salem.14@gmail.com>
2020-04-30 17:09:50 +0000

    bug fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,13 @@ if __name__ == "__main__":
 
     len_argv = len(argv) - 1
     counter = 1
+    text = "arguments"
     if len_argv == 0:
         print("{:d} arguments.".format(len_argv))
     else:
-        print("{:d} arguments:".format(len_argv))
+        if len_argv == 1:
+            text = text[:-1]
+        print("{:d} {}:".format(len_argv, text))
         for elem in argv[1:]:
             print("{:d}: {}".format(counter, elem))
             counter += 1
