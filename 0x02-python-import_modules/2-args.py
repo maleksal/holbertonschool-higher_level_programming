3e5dc7c33307216983a959f2a40e5732fabefc63
Malek Salem <malek.salem.14@gmail.com>
2020-04-30 17:02:14 +0000

    fix bug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-from sys import argv
-
 if __name__ == "__main__":
+    from sys import argv
 
     len_argv = len(argv) - 1
     counter = 1
     if len_argv == 0:
-        print("0 arguments.")
+        print("{:d} arguments.".format(len_argv))
     else:
-        print("{} arguments:".format(len_argv))
+        print("{:d} arguments:".format(len_argv))
         for elem in argv[1:]:
-            print("{}: {}".format(counter, elem))
+            print("{:d}: {}".format(counter, elem))
             counter += 1
