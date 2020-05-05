d94e4c1b7efaa70549ffd52b971780bcde8948d0
Malek Salem <malek.salem.14@gmail.com>
2020-05-05 17:58:27 +0000

    fix error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for e in i:
-            print("{:d}".format(e), end=" ")
+            print("{:d}".format(e), end="")
+            if i[-1] != e:
+                print(end=" ")
         print()
