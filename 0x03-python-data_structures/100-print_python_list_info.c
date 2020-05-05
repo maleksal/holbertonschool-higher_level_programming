cf35cc640bc05af37e8037a455d1f3dcc1a087c1
Malek Salem <malek.salem.14@gmail.com>
2020-05-05 23:13:03 +0000

    fix erros

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,6 +14,6 @@ void print_python_list_info(PyObject *p)
 	for ( ; i < length; i++)
 	{
 		obj = PyList_GetItem(p, i);
-		printf("Element %lu: %s\n", i, ((PyObject *)(p))->ob_type->tp_name);
+		printf("Element %lu: %s\n", i, Py_TYPE(obj)->tp_name);
 	}
 }
