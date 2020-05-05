a201de7f4e39f490d3d1f311871d016ab9d26377
Malek Salem <malek.salem.14@gmail.com>
2020-05-05 19:52:20 +0000

    fix error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
 
-    max_n = 0
+    max_n = my_list[0]  # Handle negative
     for i in my_list:
         if i > max_n:
             max_n = i
