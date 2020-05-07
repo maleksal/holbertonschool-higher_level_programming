d3deb6c4a0bf7eb7a38a95b0642d298b9118c33e
Malek Salem <malek.salem.14@gmail.com>
2020-05-07 19:15:22 +0000

    fix bug

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@
 def multiply_by_2(a_dictionary):
     new_dic = dict()
     for key, value in a_dictionary.items():
-        a_dictionary[key] = value * 2
+        new_dic[key] = value * 2
     return new_dic
