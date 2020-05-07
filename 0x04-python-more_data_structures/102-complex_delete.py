e66ac10491cf14f5d69c54c828acc435ad87719a
Malek Salem <malek.salem.14@gmail.com>
2020-05-07 22:00:53 +0000

    add task 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    l_keys = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            l_keys.append(key)
+
+    for key in l_keys:
+        del a_dictionary[key]
