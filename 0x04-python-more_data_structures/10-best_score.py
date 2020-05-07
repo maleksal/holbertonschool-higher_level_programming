26d4d1e397e7755ca06e45dc7a241341bf1b117f
Malek Salem <malek.salem.14@gmail.com>
2020-05-07 19:37:01 +0000

    fix errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,9 @@
 
 
 def best_score(a_dictionary):
-    if not a_dictionary or a_dictionary is {}:
+    if not a_dictionary:
         return None
-    return max(a_dictionary)
+    maxi = max(a_dictionary.values())
+    for key in a_dictionary.keys():
+        if a_dictionary[key] == maxi:
+            return key
