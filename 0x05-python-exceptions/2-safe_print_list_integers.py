2f8e317a94dda8ab6ef6bbee312b21338a399ae7
Malek Salem <malek.salem.14@gmail.com>
2020-05-18 18:10:49 +0000

    add task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    j = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+        except (ValueError,TypeError):
+            i += 1
+            j += 1
+
+    print()
+    return i - j
