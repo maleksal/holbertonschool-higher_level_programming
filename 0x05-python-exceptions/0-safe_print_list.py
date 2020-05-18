e0dfbe31083dad02d132d0518fb0892aa7d592a6
Malek Salem <malek.salem.14@gmail.com>
2020-05-18 17:44:20 +0000

    add permissions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print(my_list[i], end="")
             i += 1
-        print("\n")
+        print()
         return i
     
     except IndexError:
