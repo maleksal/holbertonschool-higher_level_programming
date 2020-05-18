f5732877be27d2facf11f09b42806e588e58b293
Malek Salem <malek.salem.14@gmail.com>
2020-05-18 19:50:52 +0000

    add task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    import sys
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as i:
+        sys.stderr.write("Exception: " + str(i))
+        return False
