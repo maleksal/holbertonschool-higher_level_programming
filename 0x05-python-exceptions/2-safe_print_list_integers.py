804d51e837c7b469a5a7eb4e416e836b4b3abf53
Malek Salem <malek.salem.14@gmail.com>
2020-05-18 18:22:19 +0000

    fix PEP8 check 6

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,10 +8,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             i += 1
-        except (ValueError,TypeError):
+        except (ValueError, TypeError):
             i += 1
             j += 1
-        finally:
-            pass
+
     print()
     return i - j
