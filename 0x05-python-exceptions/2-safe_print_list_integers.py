5fefec65a04257679f921c8ad0bdb36ef31abd5b
Malek Salem <malek.salem.14@gmail.com>
2020-05-18 18:18:22 +0000

    fix error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,5 +11,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError,TypeError):
             i += 1
             j += 1
+        finally:
+            pass
     print()
     return i - j
