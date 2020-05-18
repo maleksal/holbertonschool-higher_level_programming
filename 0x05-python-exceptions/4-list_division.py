9bb4e3a504dbd8ba17cc2c4d40a18ad80de00c30
Malek Salem <malek.salem.14@gmail.com>
2020-05-18 19:34:01 +0000

    fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,5 +18,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except(ValueError, TypeError):
             print("wrong type")
             new_list.append(0)
-
+        finally:
+            pass
     return new_list
