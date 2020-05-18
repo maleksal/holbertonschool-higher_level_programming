d5423afd2a1dd53cf85461f9396ffc1e80a70ceb
Malek Salem <malek.salem.14@gmail.com>
2020-05-18 17:41:15 +0000

    add task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        while i < x:
+            print(my_list[i], end="")
+            i += 1
+        print("\n")
+        return i
+    
+    except IndexError:
+        return i
