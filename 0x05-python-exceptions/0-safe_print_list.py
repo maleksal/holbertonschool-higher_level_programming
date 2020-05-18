6fcd23d1e688404bd9d6c6ac3be5c7913a79f304
Malek Salem <malek.salem.14@gmail.com>
2020-05-18 17:47:13 +0000

    update code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
-    try:
-        while i < x:
+
+    while i < x:
+        try:
             print(my_list[i], end="")
             i += 1
-        print()
-        return i
-    
-    except IndexError:
-        return i
+        except IndexError:
+            break
+    print()
+    return i
