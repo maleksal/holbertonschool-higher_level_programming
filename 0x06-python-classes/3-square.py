3a780d745435de5c32e3222e2fa3cdf08b0b8098
Malek Salem <malek.salem.14@gmail.com>
2020-05-19 20:13:37 +0000

    add task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+'''Square module'''
+
+
+class Square:
+    '''
+    Square class
+
+    Atributes:
+        __size (int): The size of a square is crucial for a square
+
+    '''
+
+    def __init__(self, size=0):
+        '''
+        Constructor of the Square class
+
+        Args:
+            size (int): The size of square
+
+        '''
+
+        self.__set(size)
+
+    def __get(self):
+        '''
+        Instance attributes getter
+        '''
+
+        return self.__size
+
+    def __set(self, size):
+        '''Instance attributes setter
+
+        Args:
+            size(int): an int to be assigned to size
+
+        Return:
+            TypeError: when size is not an int
+            ValueError: when size is less than 0
+
+        '''
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
+
+    def area(self):
+        ''' Compute square area
+
+        Return:
+            integer: computed square area
+
+        '''
+        return self.__size ** 2
