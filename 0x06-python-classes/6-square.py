8176d30756d65203be3ac6f6bd849a875428a7ff
Malek Salem <malek.salem.14@gmail.com>
2020-05-19 20:51:09 +0000

    add task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+
+'''Square module'''
+
+
+class Square:
+    '''
+    Square class
+
+    Atributes:
+        __size (int): The size of a square is crucial for a square
+        __position (int): Private instance attribute
+
+    '''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''
+        Constructor of the Square class
+
+        Args:
+            size (int): The size of square
+            position (int): Private instance attribute
+
+        '''
+
+        self.size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        '''
+        Instance attributes getter
+        '''
+
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        '''Instance attributes setter
+
+        Args:
+            size(int): an int to be assigned to size
+
+        Return:
+            TypeError: when size is not an int
+            ValueError: when size is less than 0
+
+        '''
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
+
+    @property
+    def position(self):
+        '''
+        Getter for position instance attribute
+
+        '''
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        '''Instance attributes setter for position
+
+        Args:
+            position (tuple): tuple of 2 integer
+
+        Return:
+            TypeError: when not a tuple of 2 positive integers
+
+        '''
+
+        if type(position) != tuple or len(tuple) != 2\
+                or position[0] < 0 or position[1] < 0:
+                    raise("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def area(self):
+        ''' Compute square area
+
+        Return:
+            integer: computed square area
+
+        '''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''
+        prints in stdout the square with the character #
+        '''
+
+        if self.__size == 0:
+            print()
+
+        print("\n" * self.__position[1], end="")
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
