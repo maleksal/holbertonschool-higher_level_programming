fd229b588978b432ccefb63030e4603cae02276d
Malek Salem <malek.salem.14@gmail.com>
2020-05-19 19:34:17 +0000

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''Square module'''
+
+
+class Square:
+    '''
+    Square class
+
+    Atributes:
+        size (int): The size of a square is crucial for a square
+
+    '''
+
+
+    def __init__(self, size):
+        '''
+        Constructor of the Square class
+
+        Args:
+            size (int): The size of square
+
+        '''
+
+        self.__size = size
