3a18143212ca4a3d962cfc15ab807eca7de45a32
Malek Salem <malek.salem.14@gmail.com>
2020-05-21 23:41:10 +0100

    fix docs

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
+"""
+Add integer module
+functions:
+    add_integers
+"""
+
 
 def add_integer(a, b=98):
-
+    """ add two integers
+    return a + b
+    """
     if not type(a) in [int, float]:
         raise TypeError("a must be an integer")
 
