0ab345437e21cbddd68181a2905cfc4c36a0f43a
Malek Salem <malek.salem.14@gmail.com>
2020-06-02 23:37:24 +0100

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class module """
+
+
+class BaseGeometry(object):
+    """ BaseGeometry class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
