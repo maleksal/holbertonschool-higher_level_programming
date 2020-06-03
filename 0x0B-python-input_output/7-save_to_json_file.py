44dbd2d6e783abada29ae919621de3f215827206
Malek Salem <malek.salem.14@gmail.com>
2020-06-03 18:25:29 +0100

    add task 7

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' I/O & Json module '''
+
+
+def save_to_json_file(my_obj, filename):
+    '''  writes an Object to a text file, using a JSON representation '''
+
+    import json
+
+    # Serialize object
+    serializer = json.dumps(my_obj)
+    # write to file
+    with open(filename, 'w+', encoding="UTF8") as file:
+        file.write(serializer)
