85b80336283622a99963a58dad90a54f063f9d6d
Malek Salem <malek.salem.14@gmail.com>
2020-06-03 20:29:01 +0100

    fix code

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 ''' python script '''
+import sys
 
-if __name__ == '__main__':
-    import sys
-    save_to_json_file = __import__('7-save_to_json_file.py').save_to_json_file
-    load_json_file = __import__('8-load_from_json_file').load_from_json_file
-    save_to = "add_item.json"
+save_to_json_file = __import__('7-save_to_json_file.py').save_to_json_file
+load_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to = "add_item.json"
 
-    if sys.argv > 0:
-        load_json_file(save_to)
-        save_to_json_file(sys.argv[1:], save_to)
+old_json = []
+if sys.argv > 0:
+    try:
+        old_json = load_json_file(save_to)
+    except FileNotFoundError:
+        pass
+    save_to_json_file(old_json + sys.argv[1:], save_to)
