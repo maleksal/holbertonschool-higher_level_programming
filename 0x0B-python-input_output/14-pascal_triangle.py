8bea7bdd4ca0ea83a3ea7e043c2c350029f19b1c
Malek Salem <malek.salem.14@gmail.com>
2020-06-03 22:34:04 +0100

    add task 13

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' pascal triangle Module '''
+
+
+def pascal_calculation(my_list):
+    ''' calculate numbers inside list '''
+    new = []
+    prevElement = 0
+    counter = 0
+
+    for i in my_list:
+        new.append(i + prevElement)
+        prevElement = i
+        counter += 1
+
+    # append last element
+    new.append(my_list[-1])
+    
+    return new
+
+
+def pascal_triangle(n):
+    ''' returns a list of lists of integers representing the Pascal’s triangle '''
+    matrix = [[1]]
+
+    for i in range(n - 1):
+        new_list = pascal_calculation(matrix[-1])
+        matrix.append(new_list)
+    return matrix
+
+print(pascal_triangle(5))
