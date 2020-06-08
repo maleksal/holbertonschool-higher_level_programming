e906d35cf1cd98d3da54ce710591b058d11de6ea
Malek Salem <malek.salem.14@gmail.com>
2020-06-08 12:01:16 +0100

    overwrite __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,7 @@ class Rectangle(Base):
     def display(self):
         for i in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
