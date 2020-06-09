226d1d13ff7af8bdebaedb6b82f7fdaea32d5fc3
Malek Salem <malek.salem.14@gmail.com>
2020-06-09 22:14:31 +0100

    add validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,18 @@ class Square(Rectangle):
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """ square size getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        ''' square size  setter '''
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value <= 0:
+            raise ValueError('size must be > 0')
+        self.width = value
+        self.height = value
