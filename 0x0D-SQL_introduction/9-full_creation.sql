344cc003c2d9f6dd3273064f34abf06d261ef280
Malek Salem <malek.salem.14@gmail.com>
2020-07-14 10:01:52 +0000

    add task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Write a script that creates a table second_table in the database hbtn_0c_0.
+-- Enjoy it!
+CREATE TABLE IF NOT EXISTS friends (
+	id INT,
+	name VARCHAR(256),
+	score INT);
+
+INSERT INTO second_table
+	(id, name, score)
+VALUES
+	(1,' John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
