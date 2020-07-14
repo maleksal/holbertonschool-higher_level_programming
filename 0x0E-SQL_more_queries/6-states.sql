28ee8975c996ed401524f7ab82d2c24c883c5cc0
Malek Salem <malek.salem.14@gmail.com>
2020-07-14 19:21:51 +0000

    add task 5

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 --  script that creates the database hbtn_0d_usa and the table states
 -- ENJOY!
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 	);
