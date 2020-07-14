303ad7fa8c9fddfeea7fc3d19c17fb2fa6384e5e
Malek Salem <malek.salem.14@gmail.com>
2020-07-14 19:32:41 +0000

    add task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 --  script that creates the database hbtn_0d_usa and the table cities
 -- ENJOY!
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL UNIQUE AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id),
