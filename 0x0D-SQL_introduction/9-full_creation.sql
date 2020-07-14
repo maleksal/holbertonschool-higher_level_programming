c562d457c72acdfbf31ad9e31b479e934f8f0c09
Malek Salem <malek.salem.14@gmail.com>
2020-07-14 10:05:21 +0000

    fix typo*

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,7 +8,9 @@ CREATE TABLE IF NOT EXISTS second_table (
 INSERT INTO second_table
 	(id, name, score)
 VALUES
-	(1,' John', 10),
+	(1,'John', 10),
 	(2, 'Alex', 3),
 	(3, 'Bob', 14),
 	(4, 'George', 8);
+
+SELECT * FROM second_table;
