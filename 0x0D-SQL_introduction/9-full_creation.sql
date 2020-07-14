cc71676fa0048c5c1071042fd4a9ab64c69c21d0
Malek Salem <malek.salem.14@gmail.com>
2020-07-14 10:03:31 +0000

    fix typo*

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Write a script that creates a table second_table in the database hbtn_0c_0.
 -- Enjoy it!
-CREATE TABLE IF NOT EXISTS friends (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT);
