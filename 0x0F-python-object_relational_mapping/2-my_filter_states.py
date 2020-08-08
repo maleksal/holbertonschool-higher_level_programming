776abc69295595f1914ec7065e9617600963277e
Malek Salem <malek.salem.14@gmail.com>
2020-08-08 23:33:40 +0000

    add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+task 2
+"""
+
+if __name__ == "__main__":
+    import MySQLdb as mysql
+    from sys import argv as args
+
+    # assign args to variables
+    mysql_username = args[1]
+    mysql_password = args[2]
+    db_name = args[3]
+    argument = args[4]
+
+    # connect to Mysql server
+    database = mysql.connect(
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name)
+
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}';\
+                    ORDER BY state.id".format(argument))
+
+    result = cursor.fetchall()
+    for i in result:
+            print(i)
