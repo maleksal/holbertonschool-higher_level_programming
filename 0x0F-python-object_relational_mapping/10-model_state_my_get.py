e89209d82ad7ecafcc2dc8b2e7537f4fc264c629
Malek Salem <malek.salem.14@gmail.com>
2020-08-09 16:35:14 +0000

    add task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+ prints the State object with the name passed
+ as argument from the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import text, create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    Tosearch = sys.argv[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(
+            State.name == Tosearch).order_by(State.id):
+        print(state.id)
+    session.close()
