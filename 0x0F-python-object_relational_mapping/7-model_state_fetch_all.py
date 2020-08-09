5411374f957c491d0dafb00e19000374127d2940
Malek Salem <malek.salem.14@gmail.com>
2020-08-09 16:00:03 +0000

    fix red checks

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
         username, password, database), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    session = sessionmaker()
-    session.configure(bind=engine)
-    session = session()
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
     for state in session.query(State).order_by(State.id).all():
         print("{}:{}".format(state.id, state.name))
     session.close()
