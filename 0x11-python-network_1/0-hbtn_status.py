ae53788bae58cc401d1bf87fc20ff1eca148b87f
Malek Salem <malek.salem.14@gmail.com>
2020-08-24 17:16:54 +0100

    add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Fetch from https://intranet.hbtn.io/status """
+
+
+def fetchWebsite():
+	import urllib.request
+
+	url = "https://intranet.hbtn.io/status"
+	
+	with urllib.request.urlopen(url) as response:
+		html = response.read()
+	print("Body response:")
+	print("\t- type: {}".format(type(html)))
+	print("\t- content: {}".format(html))
+	print("\t- utf8 content: {}".format(html.decode('utf8')))
+
+
+if __name__ == "__main__":
+	fetchWebsite()
