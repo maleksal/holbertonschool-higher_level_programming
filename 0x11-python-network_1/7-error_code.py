650d0ffbb56a96e884a2966bf6d4a5e0c50b62c9
Malek Salem <malek.salem.14@gmail.com>
2020-08-24 21:40:19 +0100

    add task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Send request """
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    response = requests.get(sys.argv[1])
+    statusCode = response.status_code
+
+    if statusCode > 400:
+        print("Error code: ", statusCode)
+    else:
+        print(response.text)
