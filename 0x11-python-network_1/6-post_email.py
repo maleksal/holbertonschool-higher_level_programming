65962c1a899592ba703580c567b4daf6a219fa19
Malek Salem <malek.salem.14@gmail.com>
2020-08-24 21:28:23 +0100

    fix bugs

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    response = requests.post(sys.argv[1])
+    response = requests.post({"email": sys.argv[1]})
     print(response.text)
