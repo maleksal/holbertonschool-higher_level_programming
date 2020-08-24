3ebd90637056bbc1cdd1eca6b366e31f7eb24fb7
Malek Salem <malek.salem.14@gmail.com>
2020-08-24 22:20:27 +0100

    fix bug

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    url = " http://0.0.0.0:5000/search_user"
+    url = " http://c0a6ac671e05.9790342a.hbtn-cod.io:5000/search_user"
 
     # take argument
     data = {"q": sys.argv[1] if len(sys.argv) > 1 else ""}
-    response = requests.post(url, params=data)
+    response = requests.post(url, data=data)
 
     if not response.json():
         print("No Result")
