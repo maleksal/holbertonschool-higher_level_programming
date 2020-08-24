9e0de6e84f2af55f17f33dfe0794860ee46810ab
Malek Salem <malek.salem.14@gmail.com>
2020-08-24 21:10:34 +0100

    add task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Handle error code """
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    response = requests.get(sys.argv[1])
+    print(response.headers["X-Request-Id"])
