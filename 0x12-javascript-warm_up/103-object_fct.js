9a5b386ef0ea5bb36db81d56fe0a0a308fafd391
Malek Salem <malek.salem.14@gmail.com>
2020-09-15 18:57:28 +0100

    fix bug

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
+  value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
-    this.value += 1;
-}
+  this.value += 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
