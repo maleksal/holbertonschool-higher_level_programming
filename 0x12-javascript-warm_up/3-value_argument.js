f3cb018a4b12d92cb9bc178b569f822d8516f1ff
Malek Salem <malek.salem.14@gmail.com>
2020-09-15 17:43:46 +0100

    add task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let count = 0;
+process.argv.forEach((val, index) => {
+  if (index >= 2) {
+    console.log(`${val}`);
+  }
+  count++;
+});
+if (count === 2) {
+  console.log('No argument');
+}
