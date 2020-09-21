e8ff2512a21340e330b23921c35403a16dd86c87
Malek Salem <malek.salem.14@gmail.com>
2020-09-21 20:57:48 +0100

    fix bug

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const arr = [];
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     arr.push(list[i]);
   }
   return arr;
