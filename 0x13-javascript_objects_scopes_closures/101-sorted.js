b71c7f222f85e9c5c7fd6993312e33d9a19772d8
Malek Salem <malek.salem.14@gmail.com>
2020-09-21 20:37:06 +0100

    add task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
 
-dictionary = module.require('./101-data.js')
+const dictionary = module.require('./test.js').dict;
+const newDic = {};
+
+for (const key in dictionary) {
+  if (newDic[dictionary[key]] !== undefined) {
+    newDic[dictionary[key]].push(key);
+  } else {
+    newDic[dictionary[key]] = [key];
+  }
+}
+console.log(newDic);
