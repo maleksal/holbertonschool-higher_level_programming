de6c3429e8e74344adfa5890745343341147ce7d
Malek Salem <malek.salem.14@gmail.com>
2020-09-22 19:34:51 +0100

    fix bugs

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,13 +3,8 @@ const fs = require('fs');
 const contentFile = process.argv[2];
 const toWrite = process.argv[3];
 
-fs.readFile(contentFile, 'utf-8', (error, text) => {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(toWrite, text, 'utf-8', (err) => {
-      if (err) { console.log(err); }
-    });
-    console.log(text);
+fs.writeFile(toWrite, contentFile, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
   }
 });
