33af122415a9a81d2a7629368a40eb33c93562e1
Malek Salem <malek.salem.14@gmail.com>
2020-09-22 19:44:01 +0100

    add task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
